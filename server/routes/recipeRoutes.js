480c0ce5567ab62d7333e6862f3003af2f7c0c96
Lucas Holter <holter@chalmers.se>
2023-09-12 13:08:06 +0200

    Add standard routes for recipe entity, is routed through userRoutes

---
+++ server/routes/recipeRoutes.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = new express.Router();
+
+router.get('/', function(req, res){
+    //Get all recipes for specific user 
+});
+
+router.get('/:name', function(req, res){
+    //Get recipe with specific name 
+});
+
+router.post('/', function(req, res){
+    //Create new recipe for a specific user
+});
+
+router.put('/:name', function(req, res){
+    //Replace recipe with specific name
+});
+
+router.patch('/:name', function(req, res){
+    //Update recipe with specific name
+});
+
+router.delete('/', function(req, res){
+    //Delete all recipes for specific user
+});
+
+router.delete('/:name', function(req, res){
+    //Delete recipe with specific name
+});
+
+
+module.exports = router;
