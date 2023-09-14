787280c6a9f20120ec389acf1bf7cef1ce20ab3a
Lucas Holter <holter@chalmers.se>
2023-09-14 17:16:04 +0200

    Adds functionallity to create, and find a specific user from database

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,17 +3,48 @@ const router = new express.Router();
 const recipeRouter = require('./recipeRoutes');
 const groceryListRouter = require('./groceryListRoutes');
 const foodItemRoutes = require('./foodItemRoutes');
+const User = require('../models/User');
+const app = require('../app');
+const mongoose = require('mongoose');
 
 router.get('/', function(req, res){
     //Get all users
 });
 
-router.get('/:username', function(req, res){
+router.get('/:username', (req, res) => {
     //Get user with specific username
+    var filter = {userName: req.params.username};
+    
+    var user = User.findOne(filter)  
+    .then((user) => {
+        if(!user){
+            res.status(404).json({message: "User not found"});
+        } 
+        else{
+            res.status(200).json(user);
+        }
+    })
+    .catch((error) => {
+        console.log(error);
+        res.status(500).json({message: "Server error"});
+    });
+
+    
 });
 
-router.post('/', function(req, res){
+router.post('/', (req, res) => {
     //Create new user
+    
+
+    var user = new User(req.body);
+    user.save()
+    .then(() => {
+        res.status(201).json({message: "User created"});
+    })
+    .catch((error) =>{
+        console.error(error);
+        res.status(500).json({message: "Server error"});
+    });
 });
 
 router.patch('/:username', function(req, res){
