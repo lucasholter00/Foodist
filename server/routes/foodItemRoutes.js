8e220707d98b3ad6ccd40677e08a5b9edaac89ff
Faiza Amjad <faizaam@student.chalmers.se>
2023-09-14 21:01:07 +0200

    #6 Added DELETE method to delete all food items

--- server/routes/foodItemRoutes.js
+++ server/routes/foodItemRoutes.js
@@ -88,7 +88,19 @@ router.patch('/:id', function (req, res) {
 
 router.delete('/', function (req, res) {
     //Delete all food items
-   
+    Food.find()
+        .then((result) => {
+            if (!result) {
+                res.status(404).json({ success: false, msg: "Foods not found", data: result });
+            } else {
+                result.delete();
+                res.status(200).json({ success: true, msg: "Food List deleted", data: result });
+            }
+        })
+        .catch((err) => {
+            console.log(err.message);
+            res.status(500).json({ message: err.message })
+        })
 });
 
 router.delete('/:id', (req, res) => {
