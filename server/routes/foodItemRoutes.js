277c28f6682af661483dba29f47e0c0952328323
Faiza Amjad <faizaam@student.chalmers.se>
2023-09-14 20:05:12 +0200

    #6 Modified get method for food list in case the list is null and status corrections

--- server/routes/foodItemRoutes.js
+++ server/routes/foodItemRoutes.js
@@ -1,81 +1,81 @@
 const express = require('express');
-const router = new express.Router({mergeParams: true});
+const router = new express.Router({ mergeParams: true });
 const Food = require("../models/User");
 
-router.get('/', (req, res)=>{
+router.get('/', (req, res) => {
     //Get all get all food items in the database
-   Food.find()
-   .then((result) => {
-    res.status(200).json({ success: true, msg: "Food List", data: result});
-   })
-   .catch((err) => {
-    console.log(err.message);
-    res.status(500).json({message: err.message})
-   })
+    Food.find()
+        .then((result) => {
+            res.status(200).json({ success: true, msg: "Food List", data: result });
+        })
+        .catch((err) => {
+            console.log(err.message);
+            res.status(500).json({ message: err.message })
+        })
 });
 
-router.get('/:id', (req, res)=>{
+router.get('/:id', (req, res) => {
     //Get food item with specific id
     const id = req.param.id;
     Food.findById(id)
-    .then(result =>{
-        if (result === null) {
-            res.status(200).json({ success: false, msg: "Food not found", data: result});
-        } else {
-            res.status(200).json({ success: true, msg: "Food by ID", data: result});
-        }
-    })
-    .catch((err) => {
-        console.log(err.message);
-        res.status(500).json({message: err.message})
-       })
+        .then(result => {
+            if (result === null) {
+                res.status(200).json({ success: false, msg: "Food not found", data: result });
+            } else {
+                res.status(200).json({ success: true, msg: "Food by ID", data: result });
+            }
+        })
+        .catch((err) => {
+            console.log(err.message);
+            res.status(500).json({ message: err.message })
+        })
 });
 
-router.post('/add-food', (req, res)=>{
+router.post('/add-food', (req, res) => {
     //Create new food item
     var food = new Food(req.body);
 
-        food.save()
-        .then((result)=>{
-            res.status(200).json({ success: true, msg: "Food details", data: food});
+    food.save()
+        .then((result) => {
+            res.status(200).json({ success: true, msg: "Food details", data: food });
         })
-        .catch((err)=>{
+        .catch((err) => {
             console.log(err.message);
-        res.status(500).json({message: err.message})
-        })       
+            res.status(500).json({ message: err.message })
+        })
 
 });
 
-router.put('/:id', function(req, res){
+router.put('/:id', function (req, res) {
     //Replace food with specific id
 });
 
-router.patch('/:id', function(req, res){
+router.patch('/:id', function (req, res) {
     //Update a specific food item
 
 });
 
-router.delete('/', function(req, res){
+router.delete('/', function (req, res) {
     //Delete all food items
 
 });
 
-router.delete('/:id', (req, res)=>{
+router.delete('/:id', (req, res) => {
     //Delete food item with specific id
     const id = req.params.id;
 
     Food.findByIdAndDelete(id)
-    .then(result =>{
-        if (result === null) {
-            res.status(200).json({ success: false, msg: "Food not found", data: result});
-        } else {
-            res.status(200).json({ success: true, msg: "Food deleted", data: result});
-        }
-    })
-    .catch((err)=>{
-        console.log(err.message);
-    res.status(500).json({message: err.message})
-    })
+        .then(result => {
+            if (result === null) {
+                res.status(200).json({ success: false, msg: "Food not found", data: result });
+            } else {
+                res.status(200).json({ success: true, msg: "Food deleted", data: result });
+            }
+        })
+        .catch((err) => {
+            console.log(err.message);
+            res.status(500).json({ message: err.message })
+        })
 
 });
 
