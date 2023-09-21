445134dced3a7018ba823277aa3ed3ac11a5bbf2
Lucas Holter <holter@chalmers.se>
2023-09-21 15:34:10 +0200

    Adds post logic to grocey list

--- server/routes/v1/groceryListRoutes.js
+++ server/routes/v1/groceryListRoutes.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const User = require("../../models/User");
 const router = new express.Router({mergeParams: true});
+const App = require("../../app");
 
 router.get('/', function(req, res){
     //Get all grocery lists for specific user
@@ -11,6 +13,21 @@ router.get('/:name', function(req, res){
 
 router.post('/', function(req, res){
     //Create new grocery list for specific user
+    var filter = {userName: req.params.username};
+    User.findOne(filter)
+    .then((user) => {
+        if(!user){
+            res.status(404).json({message: 'User not found'});
+        }
+        else{
+            const newGroceryList = req.body;
+            user.groceryList.push(newGroceryList);
+            user.save()
+            .then(() => {
+                res.status(201).json({message: 'Grocery list created'});
+            })
+        }
+     });
 });
 
 router.put('/:name', function(req, res){
