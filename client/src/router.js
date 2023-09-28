18bc360c8b4dfd4237ca49121f18277b3c976dff
Cornelia Olofsson <guscornol@student.gu.se>
2023-09-28 17:46:42 +0200

    Add CreateRecipe to router.js

--- client/src/router.js
+++ client/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import CreateRecipe from './views/CreateRecipe.vue'
 
 Vue.use(Router)
 
@@ -12,6 +13,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/create-recipe',
+      name: 'create-recipe',
+      component: CreateRecipe
     }
   ]
 })
