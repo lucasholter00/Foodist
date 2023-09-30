f14927f01d277438bc8f78001b2d319dbc09182a
Lucas Holter <holter@chalmers.se>
2023-09-30 22:28:20 +0200

    Add grocery list creation to views, also add path to router

--- client/src/router.js
+++ client/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import CreateGroceryList from './views/CreateGroceryList.vue'
 
 Vue.use(Router)
 
@@ -12,6 +13,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/groceryList/create',
+      name: 'Create grocery list',
+      component: CreateGroceryList
     }
   ]
 })
---
+++ client/src/views/CreateGroceryList.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <b-row align-h="center" align-v="center">
+      <b-col cols="8" sm="4" lg="2">
+        <p class="errorMessage" v-if="errorMessage">{{errorMessage}}</p>
+        <b-form @submit="onSubmit">
+          <b-form-group
+            id="username"
+            label="Username"
+            label-for="input-1"
+          >
+            <b-form-input
+              id="input1"
+              placeholder="Enter username"
+              v-model="form.userName"
+              type="username"
+            >
+            </b-form-input>
+          </b-form-group>
+
+          <b-form-group
+            id="password"
+            label="Password"
+            label-for="input-2"
+          >
+            <b-form-input
+              id="input2"
+              placeholder="Enter password"
+              v-model="form.password"
+              type="password"
+            >
+            </b-form-input>
+          </b-form-group>
+          <b-row align-h="between">
+            <b-button type="Submit" variant="primary">Submit</b-button>
+            <router-link to="/register"><b-button type="Register" variant="primary" >Register</b-button></router-link>
+          </b-row>
+
+        </b-form>
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+
+// import { Api } from '@/Api'
+
+export default {
+  name: 'CreateGroceryList',
+  data() {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style>
+  .errorMessage{
+    color: red;
+    font-size: 14px;
+  }
+
+</style>
