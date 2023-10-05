b29941c218d30382e7838a0bf4ba838c19d8abd4
Cornelia Olofsson <guscornol@student.gu.se>
2023-10-05 20:13:25 +0200

    add modal to show when deleting an object, some styling

--- client/src/views/Recipes.vue
+++ client/src/views/Recipes.vue
@@ -1,15 +1,26 @@
-  <template>
-    <b-container>
-      <b-row align-h="center">
-        <b-button @click="$router.push({name:'create-recipe'})" variant="primary">Add new recipe</b-button>
+<template>
+  <b-container>
+    <b-row align-h="center">
+      <b-button @click="$router.push({ name: 'create-recipe' })" variant="primary">Add new recipe</b-button>
+    </b-row>
+    <b-row class="border">
+      <b-col v-for="(recipe, index) in recipes" :key="index" cols="12" cols-sm="6" cols-md="4" class="mb-3">
+        <div class="card-wrapper">
+          <card @showDeleteModal="showDeleteModal" @editEvent="editRecipe" class="border" :displayData="recipe" />
+        </div>
+      </b-col>
+    </b-row>
+    <b-modal v-model="showModal" title="Confirm Delete" hide-footer>
+      <div>
+        <p>Are you sure you want to delete this recipe?</p>
+      </div>
+      <b-row align-h="end" class="justify-content-around">
+        <b-button variant="danger" @click="confirmDelete">Delete</b-button>
+        <b-button variant="secondary" @click="cancelDelete">Cancel</b-button>
       </b-row>
-      <b-row class="border">
-        <b-col class="border" v-for="(recipe, index) in recipes" :key="index" cols="3">
-          <card @removeEvent="removeList" @editEvent="editRecipe" class="border" :displayData="recipe" />
-        </b-col>
-      </b-row>
-    </b-container>
-  </template>
+    </b-modal>
+  </b-container>
+</template>
 
 <script>
 import { Api } from '@/Api'
@@ -29,6 +40,8 @@ export default {
     return {
       recipes: [],
       selectedRecipe: null,
+      showModal: false, // To control the visibility of the modal
+      selectedItem: null,
       errorMessage: ''
     }
   },
@@ -53,6 +66,25 @@ export default {
         .then((res) => {
           this.recipes = res.data.recipes
         })
+    },
+    showDeleteModal(item) {
+      // Set the selected item and show the modal
+      this.selectedItem = item
+      this.showModal = true
+    },
+    confirmDelete() {
+      this.removeList(this.selectedItem)
+      this.hideModal()
+    },
+    cancelDelete() {
+      // Handle cancel deletion logic here
+      // Hide the modal without performing any deletion
+      this.hideModal()
+    },
+    hideModal() {
+      // Hide the modal and clear the selected item
+      this.showModal = false
+      this.selectedItem = null
     }
   },
   created() {
@@ -61,6 +93,15 @@ export default {
 }
 </script>
 
-<style scoped>
-/* Your styles here */
+<style>
+.card-wrapper {
+  margin: 1%;
+  width: 100%;
+}
+
+@media (min-width: 768px) {
+  .card-wrapper {
+    width: calc(50% - 2%);
+  }
+}
 </style>
