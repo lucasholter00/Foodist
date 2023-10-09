11a65327dabd260135bf6d087e1217203aca2585
Cornelia Olofsson <guscornol@student.gu.se>
2023-10-09 13:58:14 +0200

    Add new navbar, user -> profile needs to be added

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,35 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
-      <router-link to="/Foods">Foods</router-link>
-      <router-link to="/recipes">Recipes</router-link>
-      <router-link to="/groceryList"> Grocery List </router-link>
-      <router-link v-if="!currentUser" to="/login"> Login </router-link>
-      <a v-else @click="logout">Log out</a>
-    </div>
+    <b-navbar toggleable="lg" type="dark"  style="background-color: mediumseagreen">
+      <b-navbar-brand to="/">
+        <span class="app-name">Foodist - <span class="tagline">track your food</span></span>
+      </b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav style="color: white">
+          <router-link to="/" class="nav-link">Home</router-link>
+          <router-link to="/Foods" class="nav-link">Foods</router-link>
+          <router-link to="/recipes" class="nav-link">Recipes</router-link>
+          <router-link to="/groceryList" class="nav-link">Grocery List</router-link>
+          <router-link to="/recipes/recommendation" class="nav-link">Recipes rec</router-link>
+        </b-navbar-nav>
+
+        <b-navbar-nav class="ml-auto">
+          <template v-if="!currentUser">
+            <router-link to="/login" class="nav-link">Login</router-link>
+          </template>
+          <template v-else>
+            <b-nav-item-dropdown right>
+              <template #button-content>
+                <em>User</em>
+              </template>
+              <b-dropdown-item href="#">Profile</b-dropdown-item>
+              <b-dropdown-item @click="logout">Sign Out</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </template>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
 
     <!-- Render the content of the current page view -->
     <router-view :currentUser="currentUser" @currentUserEvent="updateUser" :editObject="editObject" @editEvent="updateEditObject"/>
@@ -21,13 +43,13 @@
 export default {
   data() {
     return {
-      currentUser: null,
+      currentUser: '',
       editObject: null
     }
   },
   watch: {
-    currentUser(currentUser) {
-      if (this.currentUser === (null && '')) {
+    currentUser() {
+      if (this.currentUser === null) {
         this.directLogin()
       }
     }
@@ -69,6 +91,24 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+.app-name {
+  font-size: 24px; /* Adjust the size as needed */
+  color: white;
+  font-weight: bold; /* If you want it to be bold */
+}
+
+.tagline {
+  font-size: 14px !important; /* Adjust the size as needed */
+  color: white ;
+  display: inline; /* Place the tagline on a new line */
+}
+.nav-link {
+  color: white !important; /* Set link text color to white */
+}
+.nav-link:hover {
+  text-decoration: underline !important; /* Remove underline on hover */
+  color: lightgray; /* Change link color on hover */
 }
 
 </style>
