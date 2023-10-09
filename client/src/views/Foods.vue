075f970751e80258e37989bf8bada4cc0c77fac8
Cornelia Olofsson <guscornol@student.gu.se>
2023-10-09 20:04:24 +0200

    add buttons in coherent style for all entities

--- client/src/views/Foods.vue
+++ client/src/views/Foods.vue
@@ -1,8 +1,17 @@
 <template>
+  <b-container>
  <div>
-    <Btn @btn-click="toggleAddFood"
-    :text="showAddFood ? 'Close' : 'Add Food'"
-    :color="showAddFood ? 'red' : 'green' " />
+   <b-row align-h="end">
+     <b-button
+         pill
+         class="mt-2 mb-2 mx-1 buttonStyle"
+         :class="{redText: showAddFood}"
+         @click="toggleAddFood"
+         @keydown.enter.prevent
+     >
+       {{ showAddFood ? 'Close' : '+ Add Food' }}
+     </b-button>
+   </b-row>
     <div v-show="showAddFood">
       <AddFood @add-food="addFood"/>
     </div>
@@ -21,13 +30,13 @@
      </b-row>
    </b-modal>
   </div>
- </template>
+  </b-container>
+</template>
 
 <script>
 import { Api } from '@/Api'
 import AddFood from '../components/AddFood.vue'
 import BCard from '../components/BCard.vue'
-import Btn from '../components/Btn.vue'
 
 export default {
   name: 'Foods',
@@ -36,8 +45,7 @@ export default {
   },
   components: {
     AddFood,
-    BCard,
-    Btn
+    BCard
   },
   data() {
     return {
@@ -118,7 +126,22 @@ export default {
 }
 </script>
 <style scoped>
+.buttonStyle {
+  background-color: #80a28b !important;
+  border: #80a28b !important;
+}
+
+.buttonStyle:hover {
+  background-color: #80b095 !important;
+  border: #80b095 !important;
+}
+  .redText{
+    background-color: red !important;
+  }
+  .redText:hover{
+    background-color: darkred !important;
+  }
   .border {
-    border: 1px solid;
+    border: 0;
   }
 </style>
