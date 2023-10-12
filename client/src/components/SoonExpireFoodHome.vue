a2f0d66a7037fab0a5ce56225f41f126f6780d21
Cornelia Olofsson <guscornol@student.gu.se>
2023-10-12 15:07:41 +0200

    add print soon to expire foods in homepage, only foods that expire in 5 days are printed. No styling

---
+++ client/src/components/SoonExpireFoodHome.vue
@@ -0,0 +1,49 @@
+<!-- This card is BCard but without buttons, will be used as a component in other pages -->
+<script>
+export default {
+  name: 'SoonExpireFoodHome',
+  props: {
+    displayData: []
+  },
+  data() {
+    return {}
+  },
+  methods: {
+    capitalizeFirst(toBeCapitalized) {
+      return toBeCapitalized.charAt(0).toUpperCase() + toBeCapitalized.slice(1)
+    },
+    formatField(name, value) {
+      if (name === 'expiryDate' && value) {
+        const date = new Date(value)
+        const options = { day: 'numeric', month: 'short', year: 'numeric' }
+        return 'Expiry date: ' + date.toLocaleDateString(undefined, options)
+      } else {
+        return value
+      }
+    }
+
+  }
+}
+</script>
+
+<template>
+  <b-list-group-item class="mx-auto mx-2 my-12" max-width="374">
+    <!-- Header slot -->
+    <template>
+      <b-row align-h="center">
+        <div class="mt-2 ml-2"><strong>{{ displayData.name }}</strong></div>
+      </b-row>
+    </template>
+    <template>
+      <b-row v-for="(field, name) in displayData" :key="name" align-h="center">
+        <b-col>
+          <b-card-text v-if=" name === 'expiryDate'">{{formatField(name, field)}}</b-card-text>
+        </b-col>
+      </b-row>
+    </template>
+  </b-list-group-item>
+</template>
+
+<style>
+
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,22 +1,40 @@
 <template>
-  <div>
-    <b-jumbotron header="DIT342 Frontend" lead="Welcome to your DIT342 Frontend Vue.js App">
-      <b-button class="btn_message" variant="primary" v-on:click="getMessage()" >Get Message from Server</b-button>
-      <p>Message from the server:<br/>
-      {{ message }}</p>
-    </b-jumbotron>
+  <div class="border">
+    <div>
+      <b-jumbotron header="DIT342 Frontend" lead="Welcome to your DIT342 Frontend Vue.js App">
+        <b-button class="btn_message" variant="primary" v-on:click="getMessage()">Get Message from Server</b-button>
+        <p>Message from the server:<br/>{{ message }}</p>
+      </b-jumbotron>
+    </div>
+    <b-container fluid class="border">
+      <b-col v-for="(food, index) in sortedFoods" :key="index" cols="12" md="4">
+        <b-row>
+          <soonExpireFood class="highlightCard" :displayData="food" />
+        </b-row>
+      </b-col>
+    </b-container>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import { Api } from '@/Api'
+import soonExpireFoodHome from '@/components/SoonExpireFoodHome.vue'
 
 export default {
   name: 'home',
+  components: {
+    SoonExpireFood: soonExpireFoodHome
+  },
+  props: {
+    currentUser: {
+      type: String
+    }
+  },
   data() {
     return {
-      message: 'none'
+      message: 'none',
+      foods: [],
+      sortedFoods: []
     }
   },
   methods: {
@@ -28,8 +46,45 @@ export default {
         .catch(error => {
           this.message = error
         })
+    },
+    addFood() {
+      return Api.get('/v1/users/' + this.currentUser + '/food-items')
+        .then((res) => {
+          if (res.status === 200) {
+            console.log('Result ' + res.data)
+            this.foods = res.data
+          }
+        })
+        .catch((error) => {
+          if (error.response.status === 404) {
+            this.errorMessage = 'Not found'
+          } else {
+            this.errorMessage = 'Server error'
+          }
+        })
+    },
+    soonExpiredFoods() {
+      const today = new Date()
+      const soonExpiredThreshold = 5 // Adjust this threshold as needed (e.g., 5 days)
+
+      return this.foods
+        .filter((food) => {
+          const expirationDate = new Date(food.expiryDate)
+          const daysUntilExpiration = Math.ceil((expirationDate - today) / (1000 * 60 * 60 * 24))
+          return daysUntilExpiration <= soonExpiredThreshold
+        })
+        .sort((a, b) => {
+          const dateA = new Date(a.expiryDate)
+          const dateB = new Date(b.expiryDate)
+          return dateA - dateB
+        })
     }
+  },
+  created() {
+    this.getMessage()
+    this.addFood().then(() => {
+      this.sortedFoods = this.soonExpiredFoods()
+    })
   }
 }
-
 </script>
