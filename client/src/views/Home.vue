180ccc2724a2f92ca2d2d0feb854a0f3fdbb025b
Faiza Amjad <faizaam@student.chalmers.se>
2023-10-13 15:59:47 +0200

    #64 Used global style box

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -10,7 +10,7 @@
     <p class="errorMessage" v-if="errorMessage">{{errorMessage}}</p>
     <p class="message" v-if="message">{{message}}</p>
 
-  <div class="container">
+  <div class="box">
     <h2>Expired Food</h2>
     <div class="block">
     <b-row container-fluid>
@@ -23,7 +23,7 @@
       </b-row>
     </div>
   </div>
-  <div class="container">
+  <div class="box">
     <h2>Shortly Expired Food</h2>
     <div class="block">
       <b-row>
@@ -121,14 +121,6 @@ export default {
 
 <style scoped>
 
-.container {
-  margin: 30px auto;
-  overflow: auto;
-  min-height: 300px;
-  border: 3px solid #80b095 !important;
-  padding: 30px;
-  border-radius: 5px;
-}
 h2 {
   color: mediumseagreen;
 }
