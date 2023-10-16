bf36dddc0b3cb78c1c6e36f97b7a814dccde5e5a
Cornelia Olofsson <guscornol@student.gu.se>
2023-10-16 17:40:47 +0200

    Rename component and add ingredientMatches calculations

--- client/src/components/DisplayEntityHome.vue
+++ client/src/components/DisplayEntityHome.vue
@@ -1,13 +1,22 @@
 <!-- This card is BCard but without buttons, will be used as a component in other pages -->
 <script>
+
 export default {
   name: 'Card',
+  components: { },
   props: {
-    displayData: [],
-    scaleFactor: Number
+    displayData: []
   },
   data() {
     return {}
+  },
+  computed: {
+    numberOfMatches() {
+      if (this.displayData.ingredientMatches !== undefined) {
+        return `Food items in your kitchen: ${this.displayData.ingredientMatches}`
+      }
+      return ''
+    }
   },
   methods: {
     capitalizeFirst(toBeCapitalized) {
@@ -27,7 +36,7 @@ export default {
 </script>
 
 <template>
-  <b-card class="mx-auto mx-2 my-12 custom-rounded-card shadow-lg" rounded max-width="374">
+  <b-card class="mx-auto mx-2 my-12 custom-rounded-card shadow-lg" max-width="374">
     <!-- Header slot -->
     <template #header>
       <b-row align-h="between">
@@ -40,7 +49,7 @@ export default {
         <b-col v-if="Array.isArray(field)">
           <b-list-group v-for="(entry, index) in field" :key="index" >
             <b-list-group-item v-if="typeof entry ==='object'">
-              <p v-if="name === 'ingredients'"><strong>{{ entry.name }}:</strong> {{ entry.quantity * scaleFactor }} {{ entry.unit }}</p>
+              <p v-if="name === 'ingredients'"><strong>{{ entry.name }}:</strong> {{ entry.quantity}} {{ entry.unit }}</p>
             </b-list-group-item>
             <!-- -->
             <b-list-group-item v-else>
@@ -50,8 +59,9 @@ export default {
         </b-col>
         <!-- Non array slot -->
         <b-col v-else>
-          <b-card-text v-if="name !== 'name' && name !=='_id'&& name !=='reminder' && name !=='expired'">{{formatField(name, field)}}</b-card-text>
+          <b-card-text v-if="name !== 'name' && name !=='_id'&& name !=='reminder' && name !=='expired' && name !=='ingredientMatches'">{{formatField(name, field)}}</b-card-text>
         </b-col>
+        <p v-if="name === 'ingredientMatches'"><strong>{{numberOfMatches}} </strong></p>
       </b-row>
     </template>
   </b-card>
